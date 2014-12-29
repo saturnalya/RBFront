bc8be577fc78e32b912d94a87779651093f1f016
saturnalya jones <youwishyouknewmyemail@yourbutt.com>
2014-12-28 19:41:08 -0800

    add JS: enable user to click copyright to cchange color

--- resources/javascript.js
+++ resources/javascript.js
@@ -1,1 +1,8 @@
-// Your Javascript will go here!
+$(document).ready(function() {
+//  $("#copyright").click(function(){
+// 	 $(this).css('color','#04BFAD'); 
+//	});
+	document.getElementById('copyright').onclick = function () {
+		this.style.color = 'purple';
+	}
+});
